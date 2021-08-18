69394d95d08172755df35f69660cd6b37369db2f
arpit <imarpit@outlook.in>
2021-08-18 10:22:43 +0530

    Api Testing Project Django

---
+++ countryapi/countries/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import  DefaultRouter
+
+from .views import CountryViewSet
+
+
+router = DefaultRouter()
+
+router.register(r"countries",CountryViewSet)
+
+urlpatterns = [
+    path("",include(router.urls))
+]
